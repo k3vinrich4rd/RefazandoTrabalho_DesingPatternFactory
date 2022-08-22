cc42bf7c263d7320b6811c1a94aba30bda58d3fc
k3vinrich4rd <kevinrichardve@gmail.com>
2022-08-22 07:30:47 -0300

    Inserindo atualizações primárias

---
+++ src/test/java/testandoConhecimentos/pagamentoRecebimentoPatternFactory/PagamentoRecebimentoPatternFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package testandoConhecimentos.pagamentoRecebimentoPatternFactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoRecebimentoPatternFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
