9675fc0facf45365d80d6d4e074985ddb093f87e
k3vinrich4rd <kevinrichardve@gmail.com>
2022-08-22 07:47:28 -0300

    Inserindo criação do packge e da classe model

---
+++ src/main/java/testandoConhecimentos/pagamentoRecebimentoPatternFactory/model/PagamentoModel.java
@@ -0,0 +1,30 @@
+package testandoConhecimentos.pagamentoRecebimentoPatternFactory.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data // Getter, Setter e Construtores
+@Entity // Entidade — identifica ser uma tabela
+@Table(name = "registro_de_pagamento") // Tabela que será criada
+public class PagamentoModel {
+
+    @Id // Chave primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Geração sequencial
+    private Long idDoPagamento; // atributo
+
+    @Column(name = "status_pagamento", length = 100, nullable = false)
+    private String statusDoPagamento; // atributo
+
+    @Column(name = "valor_do_pagamento", length = 100, nullable = false)
+    private BigDecimal valorDoPagamento; // atributo
+
+    @Column(name = "valor_taxa_desconto", length = 100, nullable = false)
+    private BigDecimal descontoTaxa; // atributo
+
+    @Column(name = "valor_pago", length = 100)
+    private BigDecimal valorPago; // atributo
+
+
+}
